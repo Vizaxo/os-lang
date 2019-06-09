9757d6e9ddc18d8f919ba129a77af53bddb4b64f
Vizaxo <vizaxo@mailfence.com>
2019-06-09 01:22:12 +0100

    Add list and quote special forms and example assembly macro

--- src/Language.hs
+++ src/Language.hs
@@ -2,12 +2,15 @@ module Language where
 
 import Control.Monad.Except
 import Control.Monad.State
-import Control.Lens
+import Control.Lens hiding (List, _Cons)
 import Data.Map as M
+import Data.String
 
 data SpecialForm
   = Defmacro
   | Asm
+  | Quote
+  | List
   deriving Show
 
 data Term
@@ -15,6 +18,9 @@ data Term
   | Symbol String
   deriving Show
 makePrisms ''Term
+
+instance IsString Term where
+  fromString = Symbol
 
 type Symbol = String
 
@@ -38,7 +44,7 @@ data InterpError
 
 type MonadInterpreter m = (MonadState InterpState m, MonadError InterpError m)
 
-runMonadInterpreter m = runExcept (runStateT m (InterpState M.empty))
+runInterpreter m = runExcept (runStateT m (InterpState M.empty))
 
 eval :: MonadInterpreter m => Term -> m Term
 eval (Cons (f:args)) = case f of
@@ -71,12 +77,16 @@ macroexpand m@(Macro arglist body) args
 toSF :: Symbol -> Maybe SpecialForm
 toSF "defmacro" = Just Defmacro
 toSF "asm" = Just Asm
+toSF "quote" = Just Quote
+toSF "list" = Just List
 toSF _ = Nothing
 
 applySF Defmacro args = defmacro args
 applySF Asm args = mkAsm args
+applySF Quote args = pure $ Cons args
+applySF List args = Cons <$> traverse eval args
 
-mkAsm = undefined
+mkAsm args = pure (Cons args)
 
 addmacro :: MonadState InterpState m => Symbol -> Macro -> m ()
 addmacro name macro = modify (over macros (M.insert name macro))
@@ -84,8 +94,7 @@ addmacro name macro = modify (over macros (M.insert name macro))
 unit = Cons []
 
 checkArglist :: Term -> Maybe [Symbol]
-checkArglist (Cons args) = traverse (^? _Symbol) args
-checkArglist _ = Nothing
+checkArglist = traverse (^? _Symbol) <=< (^? _Cons)
 
 defmacro :: MonadInterpreter m => [Term] -> m Term
 defmacro [Symbol name, (checkArglist -> Just arglist), body]
@@ -94,5 +103,9 @@ defMacro ts = throwError (IErrBadDefMacro ts)
 
 
 
-exampleDefmacro = Cons [Symbol "defmacro", Symbol "revFuncall", Cons [Symbol "a", Symbol "b"], Cons [Symbol "b", Symbol "a"]]
-useFuncall = Cons [Symbol "revFuncall", Symbol "x", Symbol "f"]
+exampleDefmacro = Cons ["defmacro", "revFuncall", Cons ["a", "b"], Cons ["b", "a"]]
+useFuncall = Cons ["revFuncall", "x", "f"]
+exampleAsm = Cons ["asm", "xor", "eax", "ebx"]
+multipleAsm = Cons ["list", exampleAsm, exampleAsm]
+defClearRegister = Cons ["defmacro", "clearRegister", Cons ["register"], Cons ["asm", "xor", "register", "register"]]
+clearEaxEbx = Cons ["list", Cons ["clearRegister", "eax"], Cons ["clearRegister", "ebx"]]
