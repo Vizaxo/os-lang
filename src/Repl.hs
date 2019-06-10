be2fc9bf9d58b5301d5717ebed5939c5b9c534d4
Vizaxo <vizaxo@mailfence.com>
2019-06-10 02:46:59 +0100

    Add evaluation of files

--- src/Repl.hs
+++ src/Repl.hs
@@ -9,9 +9,13 @@ import Language
 data ReplError
   = ParseError LexerParserError
   | InterpError InterpError
+  | FileError
   deriving Show
 
 rep :: Text -> Either ReplError [Term]
 rep s = do
   terms <- first ParseError (parseTerms s)
   first InterpError (evalInterpreter $ traverse eval terms)
+
+evalFile :: FilePath -> IO (Either ReplError [Term])
+evalFile file = rep . pack <$> readFile file
