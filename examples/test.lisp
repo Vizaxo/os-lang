06714d69365f2e06878dd32bb060179db7e03f74
Vizaxo <vizaxo@mailfence.com>
2019-06-10 02:47:15 +0100

    Add example file defining and using macros

---
+++ examples/test.lisp
@@ -0,0 +1,15 @@
+(defmacro id (a) (eval a))
+(defmacro clearReg (reg) (list (quote xor) reg reg))
+(defmacro if (cond true false)
+  (list
+   cond
+   (asm jnz ifFalse)
+   true
+   (asm jmp ifEnd)
+   (asm ifFalse)
+   false
+   (asm ifEnd)))
+
+(if (asm cmp eax ebx)
+    (clearReg (quote eax))
+  (clearReg (quote ebx)))
