a9384f4c2cb93891514e5405db2c7196113dd489
Vizaxo <vizaxo@mailfence.com>
2019-06-11 20:41:19 +0100

    Clean up unused examples

--- examples/test.lisp
+++ examples/test.lisp
@@ -8,19 +8,4 @@
           (lambda (g)
             (f (lambda (x) ((g g) x)))))))
 
-((Z (lambda (last)
-      (lambda (x)
-        (if (cdr x)
-            (last (cdr x))
-          x))))
- '(fi fi fo fum))
 
-(def varadic (lambda args (car (cdr args))))
-
-(varadic 'a 'b 'c 'd)
-
-(def varmacro
-     (mac args
-          (cons 'quote (cons (car (cdr args)) '()))))
-
-(varmacro 'a b 'c 'd)
