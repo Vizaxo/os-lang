0734cd042239c9269ebe23f6066f349332b439d6
Vizaxo <vizaxo@mailfence.com>
2019-06-17 15:34:12 +0100

    Testing Z combinator

--- examples/test.lisp
+++ examples/test.lisp
@@ -2,12 +2,6 @@
      (mac (name val body)
           (cons (cons 'lambda (cons (cons name '()) (cons body '()))) (cons val '()))))
 
-(def Z (lambda (f)
-         ((lambda (g)
-            (f (g g)))
-          (lambda (g)
-            (f (lambda (x) ((g g) x)))))))
-
 (def list (lambda args args))
 
 (def defmacro
@@ -17,4 +11,25 @@
 (def defun
      (mac (name args body)
           (list 'def name (list 'lambda args body))))
+
+(def Z (lambda (f)
+         ((lambda (g)
+            (f (g g)))
+          (lambda (g)
+            (f (lambda x (apply (g g) x)))))))
+
+(defun swap (x y)
+  (list y x))
+
+(defun uncons (xs nilcase conscase)
+  (if xs
+      (conscase (car xs) (cdr xs))
+    nilcase))
+
+(def map
+     (Z (lambda (map)
+          (lambda (f xs)
+            (uncons xs '() (lambda (hd tl) (cons (f hd) (map f tl))))))))
+
+(map (lambda (x) (list x 'hi)) '(a b c))
 
